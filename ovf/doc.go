da85c34e77287ac00d71a081a9d75a74555a7eb3
Stephen Fox <stephen.j.fox.jr@gmail.com>
2019-01-14 10:26:50 -0500

    Fixed multiline TODO for JetBrains in 'ovf' doc.

--- ovf/doc.go
+++ ovf/doc.go
@@ -2,7 +2,8 @@
 // .ovf files.
 //
 // TODO: Be advised: Due to the limited scope of the parent project, and a
-// limitation in Golang's XML parser, it is not recommend to use this package
-// directly unless you know what you are doing. See this GitHub issue for more:
-// https://github.com/golang/go/issues/9519.
+//  limitation in Golang's XML parser, it is not recommend to use this package
+//  directly unless you know what you are doing. See this GitHub issue
+//  for more information:
+//  https://github.com/golang/go/issues/9519.
 package ovf
